db6560ca6ecee007b37b7155186cc939e2fdbeb8
cowan <cowan@DESKTOP-E1DT78U>
2022-07-20 11:39:00 +0300

    Add project files.

---
+++ WindowsFormsSayiTahminOyunu/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsSayiTahminOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOyunuBaslat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.btnTahminEt = new System.Windows.Forms.Button();
+            this.lblBilgi = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblGicikEdenMesajlar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOyunuBaslat
+            // 
+            this.btnOyunuBaslat.Location = new System.Drawing.Point(30, 12);
+            this.btnOyunuBaslat.Name = "btnOyunuBaslat";
+            this.btnOyunuBaslat.Size = new System.Drawing.Size(332, 62);
+            this.btnOyunuBaslat.TabIndex = 0;
+            this.btnOyunuBaslat.Text = "Oyunu Başlat";
+            this.btnOyunuBaslat.UseVisualStyleBackColor = true;
+            this.btnOyunuBaslat.Click += new System.EventHandler(this.btnOyunuBaslat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1-100 arasında sayı giriniz:";
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(173, 88);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi.TabIndex = 2;
+            // 
+            // btnTahminEt
+            // 
+            this.btnTahminEt.Location = new System.Drawing.Point(105, 124);
+            this.btnTahminEt.Name = "btnTahminEt";
+            this.btnTahminEt.Size = new System.Drawing.Size(197, 23);
+            this.btnTahminEt.TabIndex = 3;
+            this.btnTahminEt.Text = "Tahmin Et";
+            this.btnTahminEt.UseVisualStyleBackColor = true;
+            this.btnTahminEt.Click += new System.EventHandler(this.btnTahminEt_Click);
+            // 
+            // lblBilgi
+            // 
+            this.lblBilgi.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBilgi.Location = new System.Drawing.Point(8, 170);
+            this.lblBilgi.Name = "lblBilgi";
+            this.lblBilgi.Size = new System.Drawing.Size(400, 23);
+            this.lblBilgi.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.Lime;
+            this.progressBar1.Location = new System.Drawing.Point(8, 211);
+            this.progressBar1.Maximum = 60;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(400, 23);
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Value = 60;
+            // 
+            // lblGicikEdenMesajlar
+            // 
+            this.lblGicikEdenMesajlar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblGicikEdenMesajlar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGicikEdenMesajlar.Location = new System.Drawing.Point(8, 253);
+            this.lblGicikEdenMesajlar.Name = "lblGicikEdenMesajlar";
+            this.lblGicikEdenMesajlar.Size = new System.Drawing.Size(400, 60);
+            this.lblGicikEdenMesajlar.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 335);
+            this.Controls.Add(this.lblGicikEdenMesajlar);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblBilgi);
+            this.Controls.Add(this.btnTahminEt);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOyunuBaslat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOyunuBaslat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Button btnTahminEt;
+        private System.Windows.Forms.Label lblBilgi;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblGicikEdenMesajlar;
+    }
+}
+
---
+++ WindowsFormsSayiTahminOyunu/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsSayiTahminOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int rastgeleSayi;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //her saniye çalışacak olan event
+            //interval 1000ms (1 second) verdiğimiz için
+
+            progressBar1.Value--;
+            btnTahminEt.Text = "Tahmin Et (" + progressBar1.Value + ")";
+
+           
+            switch (progressBar1.Value)
+            {
+                case 50:
+                    lblGicikEdenMesajlar.Text = "Kesin kaybedeceksin :)))";
+                    break;
+                case 40:
+                    lblGicikEdenMesajlar.Text = "Sen kaybetmek için yarışıyosun";
+                    break;
+                case 30:
+                    lblGicikEdenMesajlar.Text = "Senden olmayacak, sen git kod yaz...";
+                    break;
+                case 20:
+                    lblGicikEdenMesajlar.Text = "Kaplumbağa daha hızlı";
+                    break;
+                case 10:
+                    lblGicikEdenMesajlar.Text = "Eeee bitti yapamadın kiii asjkhaskjgfakj";
+                    break;
+                case 0:
+                    lblBilgi.Text = "KAYBETTİİİİİN :D olmuyosa zorlama";
+                    timer1.Stop();
+                    btnTahminEt.Enabled = txtSayi.Enabled = false;
+                    progressBar1.Value = 0;
+                    break;
+            }
+            if (progressBar1.Value < 20)
+            {
+                Console.Beep(200, 2);
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnTahminEt.Enabled = false;
+            txtSayi.Enabled = false;
+        }
+
+        private void btnOyunuBaslat_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            btnTahminEt.Enabled = true;
+            txtSayi.Enabled = true;
+
+            Random rnd = new Random();
+            rastgeleSayi = rnd.Next(1, 101);
+
+            progressBar1.Value = 60;
+
+            lblBilgi.Text = "";
+
+            txtSayi.Text = "";
+            btnTahminEt.Text = "Tahmin Et";
+
+            
+
+        }
+
+        private void btnTahminEt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSayi.Text))
+            {
+                lblBilgi.Text = "Sayi girmeyi unuttun...";
+            }
+            else
+            {
+                int sayi = 0;
+                try
+                {
+                    sayi = int.Parse(txtSayi.Text);
+
+                    if (sayi < rastgeleSayi)
+                    {
+                        lblBilgi.Text = "Küçük sayi girdin.";
+                    }
+                    else if (sayi > rastgeleSayi)
+                    {
+                        lblBilgi.Text = "Büyük sayi girdin.";
+                    }
+                    else
+                    {
+                        lblBilgi.Text = "KAZANDIN.";
+                        timer1.Stop();
+                        btnTahminEt.Enabled = txtSayi.Enabled = false;
+
+                        
+
+                        
+
+                        if(progressBar1.Value > 50)
+                        {
+                            lblBilgi.Text = "Helal fena değilsin";
+                        }
+                        else if (progressBar1.Value > 40)
+                        {
+                            lblBilgi.Text = "Yaaaaaniii";
+                        }
+                        else if (progressBar1.Value > 30)
+                        {
+                            lblBilgi.Text = "Bence kötüydün sence?";
+                        }
+                        else if (progressBar1.Value > 20)
+                        {
+                            lblBilgi.Text = "Oyun zor değil sen kötüsün";
+                        }
+                        else if (progressBar1.Value > 10)
+                        {
+                            lblBilgi.Text = "Ahahahahahah hiç bilmeseydin";
+                        }
+                        progressBar1.Value = 0;
+                    }
+
+                }
+                catch (Exception)
+                {
+                    lblBilgi.Text = "Sayi girmeyi unuttun...";
+
+                }
+            }
+        }
+
+        
+
+
+    }
+}
